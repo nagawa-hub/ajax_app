57948ebdb92b4752fe6710276c260059ba8c5333
nagawa-hub <nakagawayu2008@gmail.com>
2020-10-22 10:23:58 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'posts', to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
+  # [HTTPメソッド] '[URIパターン]', to: '[コントローラー名]#[アクション名]'
+end
